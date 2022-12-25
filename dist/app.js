54ebbae5a179bdabde97da5fb174ace0fd660bc4
faizulislamfair <faizulislamfair@gmail.com>
2022-12-26 00:18:50 +0600

    primitive types

--- dist/app.js
+++ dist/app.js
@@ -2,3 +2,17 @@
 var message = "Hello TypeScript";
 var msg = 'Remember JavaScript?';
 console.log(message, msg, language);
+// let myName: string = 'Fair';
+// myName = 'Faizul';
+// let age: number;
+// age = 55;
+// age = 155;
+// let isPokemon: boolean;
+// isPokemon = true;
+// union types
+var category;
+category = 25;
+category = 'Pokemon';
+category = false;
+category = {};
+category = [];
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,41 @@
 const message = "Hello TypeScript";
 const msg = 'Remember JavaScript?';
 console.log(message, msg, language);
+
+
+
+// let myName: string = 'Fair';
+
+// myName = 'Faizul';
+
+
+
+// let age: number;
+
+// age = 55;
+
+// age = 155;
+
+
+// let isPokemon: boolean;
+
+// isPokemon = true;
+
+
+// union types
+
+let category: number | string | boolean | [] | {};
+
+category = 25;
+
+category = 'Pokemon';
+
+category = false;
+
+category = {};
+
+category = [];
+
+
+
+
