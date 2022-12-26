00360de78ebb366b3645c3dd28baab9bac46d3fc
faizulislamfair <faizulislamfair@gmail.com>
2022-12-26 18:32:55 +0600

    object and enums

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,6 @@
 "use strict";
-var message = "Hello TypeScript";
-var msg = 'Remember JavaScript?';
+// const message = "Hello TypeScript";
+// const msg = 'Remember JavaScript?';
 // console.log(message, msg, language);
 // let myName: string = 'Fair';
 // myName = 'Faizul';
@@ -20,5 +20,41 @@ var msg = 'Remember JavaScript?';
 // pokemons.push('Giratina');
 // const newPokemons = pokemons.map((pokemon, i) => i + 1 + '. ' + pokemon);
 // console.log(pokemons, newPokemons);
-var colors;
-colors = ['Cyan', 'Crimson', 555, true, 'Grey'];
+// let colors: [string, string, number, boolean, string];
+// colors = ['Cyan', 'Crimson', 555, true, 'Grey'];
+// let pokemon: {
+//     name: string;
+//     weight?: number,
+//     type: string;
+//     megaForm: boolean;
+//     moves?: string[];
+// };
+// pokemon = {
+//     name: 'Dragonite',
+//     weight: 210,
+//     type: 'dragon',
+//     megaForm: true,
+//     moves: ['hyper beam']
+// };
+// pokemon = {
+//     name: 'Dragonite',
+//     // weight: 103,
+//     type: 'dragon',
+//     megaForm: true,
+//     moves: ['draco meteor']
+// };
+// pokemon.megaForm = false;
+// pokemon.moves = pokemon?.moves?.map(move => move.toUpperCase())
+// console.log(pokemon, pokemon.type, pokemon['moves']);
+var Friends;
+(function (Friends) {
+    Friends[Friends["Duck"] = 0] = "Duck";
+    Friends[Friends["Chicken"] = 1] = "Chicken";
+    Friends[Friends["Penguin"] = 2] = "Penguin";
+})(Friends || (Friends = {}));
+var Configs;
+(function (Configs) {
+    Configs["API_Key"] = "api_key";
+    Configs["Domain_Name"] = "domain_name";
+})(Configs || (Configs = {}));
+console.log(Friends.Penguin);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-const message = "Hello TypeScript";
-const msg = 'Remember JavaScript?';
+// const message = "Hello TypeScript";
+// const msg = 'Remember JavaScript?';
 // console.log(message, msg, language);
 
 
@@ -48,7 +48,56 @@ const msg = 'Remember JavaScript?';
 // console.log(pokemons, newPokemons);
 
 
-let colors: [string, string, number, boolean, string];
+// let colors: [string, string, number, boolean, string];
 
-colors = ['Cyan', 'Crimson', 555, true, 'Grey'];
+// colors = ['Cyan', 'Crimson', 555, true, 'Grey'];
 
+
+
+// let pokemon: {
+//     name: string;
+//     weight?: number,
+//     type: string;
+//     megaForm: boolean;
+//     moves?: string[];
+// };
+
+// pokemon = {
+//     name: 'Dragonite',
+//     weight: 210,
+//     type: 'dragon',
+//     megaForm: true,
+//     moves: ['hyper beam']
+// };
+
+
+// pokemon = {
+//     name: 'Dragonite',
+//     // weight: 103,
+//     type: 'dragon',
+//     megaForm: true,
+//     moves: ['draco meteor']
+// };
+
+
+
+
+// pokemon.megaForm = false;
+
+// pokemon.moves = pokemon?.moves?.map(move => move.toUpperCase())
+
+// console.log(pokemon, pokemon.type, pokemon['moves']);
+
+
+enum Friends {
+    Duck,
+    Chicken,
+    Penguin
+}
+
+enum Configs {
+    API_Key = "api_key",
+    Domain_Name = "domain_name"
+}
+
+console.log(Friends.Penguin);
